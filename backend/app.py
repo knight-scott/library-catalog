f6e816946153894b6c3f622d4f8a9dfdf9b0f3f9
Knight <148942757+knight-scott@users.noreply.github.com>
2024-12-29 07:11:53 +0000

    adjust flask port and container

--- backend/app.py
+++ backend/app.py
@@ -6,10 +6,10 @@ from routes.books import books_bp
 app = Flask(__name__)
 
 # Register blueprints
-app.register_blueprint(boks_bp, url_prefix='/api/books')
+app.register_blueprint(books_bp, url_prefix='/api/books')
 # uncomment when libraries blueprint is complete
 # app.register_blueprint(libraries_bp, url_prefix='/api/libraries')
 # continue for genres and users
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
