da718aaa1ec71c332041032250bc0f8bf6d13fe1
Knight <148942757+knight-scott@users.noreply.github.com>
2024-12-29 05:37:03 +0000

    Change Flask API port to prevent conflicts

--- backend/app.py
+++ backend/app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 # Register blueprints
 app.register_blueprint(boks_bp, url_prefix='/api/books')
-app.register_blueprint(libraries_bp, url_prefix='/api/libraries')
+# uncomment when libraries blueprint is complete
+# app.register_blueprint(libraries_bp, url_prefix='/api/libraries')
 # continue for genres and users
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
